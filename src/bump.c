c0179df4bde600ec9909c2d22a56c8419d28c8ac
ahetru <axel.a.hetru@gmail.com>
2026-06-06 12:13:40 +0200

    refactor: dont change size value in allocator

--- src/bump.c
+++ src/bump.c
@@ -23,26 +23,24 @@ void init_bump_alloc(bump_allocator_t *a, uint8_t *heap_start)
 void *bump_alloc(bump_allocator_t *a, size_t n)
 {
   size_t alignement = (n + 7) & ~7;
-  if (alignement > a->size)
+  if (alignement + a->offset > a->size)
     return NULL;
   a->prev_offset = a->offset;
   a->offset += alignement;
-  a->size -= alignement;
   return (void *)a->heap + a->prev_offset;
 }
 
 static void  bump_alloc_info(bump_allocator_t *a)
 {
-  printf("heap: %p\nsize: %lu\noffset: %p\n", a->heap, a->size, a->heap + a->offset);
+  printf("heap addr: %p\noffset: %lu\nprev offset: %lu\nsize: %lu\n", a->heap, a->offset, a->prev_offset, a->size);
 }
 
 int main(void)
 {
   bump_allocator_t a;
   init_bump_alloc(&a, heap);
-  bump_alloc_info(&a);
 
-  while (bump_alloc(&a, 660))
+  while (bump_alloc(&a, 2048))
     bump_alloc_info(&a);
   return 0;
 }
